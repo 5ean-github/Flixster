65c8bdd2c1ec6f69bb8ce98a07a94658746a3c9c
seanchen-ai <61416948+seanchen-ai@users.noreply.github.com>
2021-09-20 20:48:10 -0700

    flixster

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  flixster
+//
+//  Created by s̶e̵a̷n̶ on 2021/9/19.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
